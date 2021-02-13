56ae7f75e6fee11fc6b31e46e6b099ffd0250c5a
Fenrir7Asteron <b.fedotov@innopolis.ru>
2021-02-13 20:15:18 +0300

    Basic LSTM model.

--- tagger_predict.py
+++ tagger_predict.py
@@ -7,9 +7,53 @@ import numpy as np
 import pickle
 
 
+def load_test_data(path):
+    test_data = []
+    with open(path, "r") as input_file:
+        lines = input_file.read().splitlines()
+        for line in lines:
+            test_data.append(line.split(' '))
+    return test_data
+
+
+def preprocess_test_data(test_data):
+    for i in range(len(test_data)):
+        sent = test_data[i]
+        for j in range(len(sent)):
+            word = test_data[i][j]
+            if word not in word_to_ix:
+                new_word = random.choice(list(word_to_ix.keys()))
+                test_data[i][j] = new_word
+
+
 def tag_sentence(test_file, model_file, out_file):
-    # write your code here. You can add functions as well.
-    # use torch library to load model_file
+    test_data_raw = load_test_data(test_file)
+    test_data = preprocess_test_data(test_data_raw)
+    model = model.load_state_dict(torch.load(model_file))
+
+    model.zero_grad()
+    model.eval()
+    test_result = []
+    with torch.no_grad():
+        for sentence in test_data:
+            inputs = prepare_sequence(sentence, word_to_ix)
+            tag_scores = model(inputs)
+            outputs = torch.argmax(tag_scores, dim=1).tolist()
+            tags = [ix_to_tag[w] for w in outputs]
+            test_result.append(tags)
+
+    with open(out_file, "w") as output_file:
+        output_file.truncate(0)
+        for i in range(len(test_data)):
+            sentence = test_data[i]
+            output_line = ""
+            for j in range(len(sentence)):
+                word = sentence[j]
+                tag = test_result[i][j]
+                output_line += '/'.join([word, tag]) + ' '
+                print('/'.join([word, tag]), end=" ")
+            output_file.write(output_line + "\n")
+
     print('Finished...')
 
 
--- tagger_train.py
+++ tagger_train.py
@@ -11,9 +11,107 @@ import torch.nn.functional as F
 import torch.optim as optim
 
 
+EMBEDDING_DIM = 64
+HIDDEN_DIM = 64
+EPOCH_NUM = 20
+
+
+def prepare_sequence(seq, to_ix):
+    idxs = [to_ix[w] for w in seq]
+    return torch.tensor(idxs, dtype=torch.long)
+
+
+def load_data(path):
+    training_data = []
+    with open(path, "r") as input_file:
+        lines = input_file.read().splitlines()
+        for line in lines:
+            sentences = []
+            tags = []
+            for token in line.split(' '):
+                split = token.split('/')
+                sentences += split[:-1]
+                tags += [split[-1]] * len(split[:-1])
+            training_data.append((sentences, tags))
+
+    word_to_ix = {}
+    tag_to_ix = {}
+    ix_to_tag = {}
+
+    for sentence, tags in training_data:
+        for word in sentence:
+            if word not in word_to_ix:
+                word_to_ix[word] = len(word_to_ix)
+        for tag in tags:
+            if tag not in tag_to_ix:
+                ix_to_tag[len(tag_to_ix)] = tag
+                tag_to_ix[tag] = len(tag_to_ix)
+
+    print(word_to_ix)
+    print(tag_to_ix)
+    print(ix_to_tag)
+
+    return training_data, word_to_ix, tag_to_ix, ix_to_tag
+
+
+class LSTMTagger(nn.Module):
+    def __init__(self, embedding_dim, hidden_dim, vocab_size, tagset_size):
+        super(LSTMTagger, self).__init__()
+        self.hidden_dim = hidden_dim
+
+        self.word_embeddings = nn.Embedding(vocab_size, embedding_dim)
+
+        # The LSTM takes word embeddings as inputs, and outputs hidden states
+        # with dimensionality hidden_dim.
+        self.lstm = nn.LSTM(embedding_dim, hidden_dim, dropout=0.1)
+
+        # The linear layer that maps from hidden state space to tag space
+        self.hidden2tag = nn.Linear(hidden_dim, tagset_size)
+
+    def forward(self, sentence):
+        embeds = self.word_embeddings(sentence)
+        lstm_out, _ = self.lstm(embeds.view(len(sentence), 1, -1))
+        tag_space = self.hidden2tag(lstm_out.view(len(sentence), -1))
+        tag_scores = F.log_softmax(tag_space, dim=1)
+        return tag_scores
+
+
 def train_model(train_file, model_file):
-    # write your code here. You can add functions as well.
-    # use torch library to save model parameters, hyperparameters, etc. to model_file
+    training_data, word_to_ix, tag_to_ix, ix_to_tag = load_data(train_file)
+
+    model = LSTMTagger(EMBEDDING_DIM, HIDDEN_DIM, len(word_to_ix), len(tag_to_ix))
+    loss_function = nn.NLLLoss()
+    optimizer = optim.SGD(model.parameters(), lr=0.1)
+
+    # See what the scores are before training
+    # Note that element i,j of the output is the score for tag j for word i.
+    # Here we don't need to train, so the code is wrapped in torch.no_grad()
+    with torch.no_grad():
+        inputs = prepare_sequence(training_data[0][0], word_to_ix)
+        tag_scores = model(inputs)
+        print(tag_scores)
+
+    for epoch in range(EPOCH_NUM):
+        for sentence, tags in training_data:
+            model.zero_grad()
+
+            sentence_in = prepare_sequence(sentence, word_to_ix)
+            targets = prepare_sequence(tags, tag_to_ix)
+
+            tag_scores = model(sentence_in)
+
+            loss = loss_function(tag_scores, targets)
+            loss.backward()
+            optimizer.step()
+        print("Epoch #{} passed. Saving to the model file.".format(epoch))
+
+    # See what the scores are after training
+    with torch.no_grad():
+        inputs = prepare_sequence(training_data[0][0], word_to_ix)
+        tag_scores = model(inputs)
+        print(tag_scores)
+
+    torch.save(model.state_dict(), model_file)
 
     print('Finished...')
 
